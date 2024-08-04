b443160a1aa0aeb16ed3b396855fe8cdb890f6d2
aledraken <153851162+aledraken@users.noreply.github.com>
2024-08-04 11:18:24 +0200

    Update pacman-packages.sh

--- pacman-packages.sh
+++ pacman-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S tldr fish gvim htop xorg-xinit xorg-server ly qtile kitty yazi rofi alsa-utils gufw git p7zip exfat-utils
+sudo pacman -S tldr fish gvim htop xorg-xinit xorg-server ly qtile kitty yazi rofi alsa-utils gufw git p7zip exfat-utils fuse2
