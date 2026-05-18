0c456b62405da7c37d86af0dc2e8895e01bdd8e4
aledraken <alefrancesco5@proton.me>
2026-05-18 11:36:05 +0200

    Temp packages

---
+++ packages.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+pac=""
+
+pac="$pac qtile xorg-xwayland python-pyxdg python-dbus-fast dunst xdg-utils xdg-desktop-portal-gtk lxqt-policykit"
+
+pac="$pac fish starship foot otf-monaspace-nerd noto-fonts ly wl-clipboard"
+
+pac="$pac bat eza git lazygit htop tldr man-db trash-cli yazi"
+
+pac="$pac fwupd udisks2 thermald brightnessctl ufw"
+
+pac="$pac pipewire wireplumber wiremix pipewire-alsa pipewire-jack pipewire-pulse"
+
+case "$(hostnamectl chassis)" in
+	"laptop")
+		pac="$pac brightnessctl"
+		;;
+esac
+
+if [ "$(hostnamectl chassis)" != "vm" ]; then
+	pac="$pac bluez bluez-utils"
+fi
+
+sudo pacman -S --needed $pac
+
+yay -S --needed auto-cpufreq
+
+sudo systemctl enable --now thermald ufw ly@tty2 auto-cpufreq
+
+sudo ufw enable
+sudo ufw logging off
